639ec6c00ae0731640baafbf72cdd94caf945944
David Pérez <dvpl88@gmail.com>
2022-05-12 20:19:55 +0200

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+class userController {
+  signup = (request, response, next) => {
+    response.json('User signup completed');
+  };
+}
+
+const UserController = new userController();
+
+export default UserController;
