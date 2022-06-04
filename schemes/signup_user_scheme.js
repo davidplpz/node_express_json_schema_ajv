27697be9bdce30fd6ef4423adf45fac5c75de1c6
David Pérez <dvpl88@gmail.com>
2022-06-04 16:54:33 +0200

    changelog and currency on payment

--- schemes/signup_user_scheme.js
+++ schemes/signup_user_scheme.js
@@ -1,24 +1,23 @@
-import SchemeValidator from "../scheme-validator.js";
+import SchemeValidator from '../scheme-validator.js';
 
 const signupScheme = {
-  $schema: "https://json-schema.org/draft/2020-12/schema",
-  title: "Signup User",
-  description: "User data for registration",
-  type: "object",
+  title: 'Signup User',
+  description: 'User data for registration',
+  type: 'object',
   properties: {
     email: {
-      description: "User email",
-      type: "string",
-      format: "email",
+      description: 'User email',
+      type: 'string',
+      format: 'email',
     },
     password: {
-      description: "User password",
-      type: "string",
-      format: "password",
+      description: 'User password',
+      type: 'string',
+      format: 'password',
       minLength: 8,
     },
   },
-  required: ["email", "password"],
+  required: ['email', 'password'],
 };
 
 const signupValidator = SchemeValidator.compile(signupScheme);
